70f7da310423233133166ee80f81fa4a35f65790
nicosimm <23009210@myrp.edu.sg>
2024-12-03 14:37:15 +0800

    L7 CAG1

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,41 @@ function App() {
 
   const handleAddMovie = (title) => {
     // TODO: Implement adding a new movie to the list
+    const NewMovie = { 
+      id: movies.length + 1, 
+      title, 
+      watched: false 
+    };
+    setMovies((movie)=>[...movie, NewMovie]);
   };
 
   const handleToggleWatched = (id) => {
     // TODO: Implement toggling a movie's watched status
+    const updateMovies = movies.map((movie) => {
+      if (movie.id === id) {
+        return { ...movie, watched: !movie.watched };
+      }
+      return movie;
+    });
+    setMovies(updateMovies);
   };
 
   const handleDeleteMovie = (id) => {
     // TODO: Implement deleting a movie from the list
+    setMovies(movies.filter((movie) => movie.id !== id)) ;
   };
 
   return (
     <div>
       <h1>Favorite Movies</h1>
       {/* TODO: Add AddMovieForm Component */}
+      <AddMovieForm onAddMovie={handleAddMovie} />
       {/* TODO: Add MovieList Component */}
+      <MovieList
+        movies={movies}
+        onToggleWatched={handleToggleWatched}
+        onDeleteMovie={handleDeleteMovie}
+      />
     </div>
   );
 }
